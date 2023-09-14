62f9da3d2b2a586cc506014bf84a5982e4668308
koringascp <koringajpgames@gmail.com>
2023-09-14 16:19:39 -0300

    share on all

--- saiyd-web.py
+++ saiyd-web.py
@@ -1972,4 +1972,5 @@ with gr.Blocks(title="RVC WebUI") as app:
             inbrowser=not config.noautoopen,
             server_port=config.listen_port,
             quiet=True,
+            share=True,
         )
